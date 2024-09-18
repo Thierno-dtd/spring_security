7c0cbf7a4a78711ac8245b8ddaca86eb3d92c572
Thierno-dtd <davidouthe2@gmail.com>
2024-09-18 16:35:02 +0000

    Configurer une authentification basée sur des utilisateurs en base de données

---
+++ src/main/java/com/springsecurity/springsecurity/configuration/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.springsecurity.springsecurity.configuration;
+
+import com.springsecurity.springsecurity.model.DBUser;
+import com.springsecurity.springsecurity.repository.DBUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private DBUserRepository dbUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        DBUser user = dbUserRepository.findByUsername(username);
+        return new User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+
+}
